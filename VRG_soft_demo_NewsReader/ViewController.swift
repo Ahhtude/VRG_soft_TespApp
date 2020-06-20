21db88be18a7fc9bc2fdb6f27e1b8355e5af46d1
Sergey <whteahhtude@gmail.com>
2020-06-20 12:23:58 +0300

    Initial Commit

---
+++ VRG_soft_demo_NewsReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VRG_soft_demo_NewsReader
+//
+//  Created by Sergey berdnik on 20.06.2020.
+//  Copyright © 2020 Sergey berdnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
